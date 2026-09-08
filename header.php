16de20a871a8f50739337a00dfe780f86d4fbb72
ChillibyteUK <dan@chillibyte.co.uk>
2026-09-08 15:07:38 +0100

    header

--- header.php
+++ header.php
@@ -10,7 +10,70 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php wp_head(); ?>
+		<link rel="preload"
+		href="<?= esc_url( get_stylesheet_directory_uri() . '/fonts/roboto-v51-latin-regular.woff2' ); ?>"
+		as="font" type="font/woff2" crossorigin="anonymous">
+	<link rel="preload"
+		href="<?= esc_url( get_stylesheet_directory_uri() . '/fonts/roboto-v51-latin-700.woff2' ); ?>"
+		as="font" type="font/woff2" crossorigin="anonymous">
+	<link rel="preload"
+		href="<?= esc_url( get_stylesheet_directory_uri() . '/fonts/roboto-v51-latin-900.woff2' ); ?>"
+		as="font" type="font/woff2" crossorigin="anonymous">
+
+	<?php
+	if ( ! is_user_logged_in() ) {
+		if ( get_field( 'ga_property', 'options' ) ) {
+			?>
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+			<script async
+				src="<?= esc_url( 'https://www.googletagmanager.com/gtag/js?id=' . get_field( 'ga_property', 'options' ) ); ?>">
+			</script>
+			<script>
+				window.dataLayer = window.dataLayer || [];
+
+				function gtag() {
+					dataLayer.push(arguments);
+				}
+				gtag('js', new Date());
+				gtag('config',
+					'<?= esc_js( get_field( 'ga_property', 'options' ) ); ?>'
+				);
+			</script>
+			<?php
+		}
+		if ( get_field( 'gtm_property', 'options' ) ) {
+			?>
+			<!-- Google Tag Manager -->
+			<script>
+				(function(w, d, s, l, i) {
+					w[l] = w[l] || [];
+					w[l].push({
+						'gtm.start': new Date().getTime(),
+						event: 'gtm.js'
+					});
+					var f = d.getElementsByTagName(s)[0],
+						j = d.createElement(s),
+						dl = l != 'dataLayer' ? '&l=' + l : '';
+					j.async = true;
+					j.src =
+						'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+					f.parentNode.insertBefore(j, f);
+				})(window, document, 'script', 'dataLayer',
+					'<?= esc_js( get_field( 'gtm_property', 'options' ) ); ?>'
+				);
+			</script>
+			<!-- End Google Tag Manager -->
+			<?php
+		}
+	}
+	if ( get_field( 'google_site_verification', 'options' ) ) {
+		echo '<meta name="google-site-verification" content="' . esc_attr( get_field( 'google_site_verification', 'options' ) ) . '" />';
+	}
+	if ( get_field( 'bing_site_verification', 'options' ) ) {
+		echo '<meta name="msvalidate.01" content="' . esc_attr( get_field( 'bing_site_verification', 'options' ) ) . '" />';
+	}
+	< ? php wp_head();
+	?>
 </head>
 
 <body <?php body_class(); ?>>
